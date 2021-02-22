9b518e8a4b5588537594aa588bb4be6c78d5dbab
Samet Akcalar <sametegitimler@gmail.com>
2021-02-22 16:31:47 +0300

    Clock Funcation Example

---
+++ Clock Funcation/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <math.h>
+//clock_t clock(void)  //tick count
+
+int main(void)
+{
+  
+	int a = 10, b = 20;
+
+	double x;
+	printf("Hesaplaniyor...");
+	clock_t start = clock();
+	for (int i = 0; i < 200'000'000; ++i)
+		x = sqrt(pow(i, 6));
+	clock_t end = clock();
+	clock_t res = end - start;
+	printf("%f seconds\n", (double)res / CLOCKS_PER_SEC);
+}
