95c67e0f22325f90f9e789c128941f58b0f3e83d
Samet Akcalar <sametegitimler@gmail.com>
2021-02-23 15:05:34 +0300

    isAtBegin and isAtEnd funcations

---
+++ isAtBegin ve isAtEnd/main.c
@@ -0,0 +1,33 @@
+﻿
+#include <stdio.h>
+#include <string.h>
+
+int isAtBegin(const char* s1, const char* s2)  
+{
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+
+	if (len2 > len1)
+		return 1;
+	if(strncmp(s1, s2, len2) == 0)
+		return 0;
+
+	return 1;
+}
+int isAtEnd(const char* s1, const char* s2)
+{
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+
+	if (len2 > len1)
+		return 1;
+	if (strcmp(s1+len1 -len2, s2) == 0)
+		return 0;
+
+	return 1;
+}
+
+int main(void)
+{
+	printf("%d", isAtEnd("kocaeli", "eli"));
+}
